e3c2d0e73b928f4b9f442476da6d758ec7699f3b
Sam Carrillo <scarrillo578@gmail.com>
2018-08-08 12:56:16 -0500

    changed beanfactory to an Applcation context, property & constructor arg tags added to spring.xml, String and primitive dependancy injection

--- src/Drawing.java
+++ src/Drawing.java
@@ -1,6 +1,5 @@
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Drawing {
     public static void main(String[] args) {
@@ -11,8 +10,9 @@ public class Drawing {
         //with spring
         //note how we are not doing a 'new Triangle();'
         //will be more useful with more complicated objects
-        BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
-        Triangle triangle = (Triangle) factory.getBean("triangle");//the xml id
+        //BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");//do not have to make filesystemresource
+        Triangle triangle = (Triangle) context.getBean("triangle");//the xml id
         triangle.draw();
     }
 }
--- src/Triangle.java
+++ src/Triangle.java
@@ -1,6 +1,39 @@
 public class Triangle {
 
+    private String type;
+    private int height;
+
+    public Triangle(String _type) {
+        //only takes type
+        this.type = _type;
+    }
+
+    public Triangle(int height) {
+        //only takes height
+        this.height = height;
+    }
+
+    public Triangle(String _type, int _height) {
+        //takes both type and height
+        this.type = _type;
+        this.height = _height;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    //commenter out to prove the constructor arg does not use setType
+    //constructor arg tag in spring.xml only uses getter methods
+    //public void setType(String type) {
+    //    this.type = type;
+    //}
+
     public void draw() {
-        System.out.println("Triangle Drawn");
+        System.out.println(getType() + " " + "Triangle Drawn" + " " + "of height: " + getHeight());
     }
 }
